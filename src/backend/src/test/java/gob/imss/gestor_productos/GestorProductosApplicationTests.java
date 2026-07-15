d1055528ff7c3a91985a2e40f3341d17060a3eb0
Larcade <kevelev1589@gmail.com>
2026-07-15 04:03:42 -0500

    feat: actualizacion backend

--- src/backend/src/test/java/gob/imss/gestor_productos/GestorProductosApplicationTests.java
+++ src/backend/src/test/java/gob/imss/gestor_productos/GestorProductosApplicationTests.java
@@ -2,12 +2,14 @@ package gob.imss.gestor_productos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles; // <-- Importar esto
 
 @SpringBootTest
+@ActiveProfiles("test") // <-- Agregar esto
 class GestorProductosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
